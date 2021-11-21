7fd1e5c3721d07923f130f9ad64b7f25e26baad8
Jenny Chau <jnechau@gmail.com>
2021-11-20 23:26:16 -0500

    commit tree 67f115be0d4c061d839c48b821492aebdb0a7040

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const alements = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+  return <nav>{alements}</nav>;
 }
 
 export default NavBar;
